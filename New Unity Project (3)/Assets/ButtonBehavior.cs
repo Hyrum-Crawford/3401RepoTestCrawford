8cf39533609df80bfa83513384d32101258aba43
Hyrum-Crawford <hc342918@scc-mmlab-03.local>
2020-02-10 16:46:34 -0500

    Added Unity Files
    Added the Unity files to the repository.

---
+++ New Unity Project (3)/Assets/ButtonBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour
+{
+
+    public AudioSource buttonAudioSource;
+
+
+    public void OnButtonClicked()
+    {
+        Debug.Log("Button Clicked!");
+
+        if (buttonAudioSource.isPlaying)
+        {
+            buttonAudioSource.Stop();
+        }
+        else
+        {
+            buttonAudioSource.Play();
+        }
+    }
+}
